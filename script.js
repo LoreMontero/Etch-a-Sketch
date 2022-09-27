6b737e108824c39d20d95257985fc5f63a7f3bff
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-09-27 19:10:46 -0400

    Change the max value in the for loop.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const sketchPad = document.querySelector('.sketchPad')
+const createDivs = (size) => {
+    const sketchPad = document.querySelector('.sketchPad');
+    sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketchPad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    for(let i = 0; i < 400; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = "blue";
+        sketchPad.insertAdjacentElement("beforeend", square);
+    }
+}
 
+createDivs(20)
